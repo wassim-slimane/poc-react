ad400c46710aea6eb0d0029b3d7491150c2c8b5f
= <=>
2021-12-07 10:02:52 +0100

    feat all

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HeroSection from '../HeroSection'
+import '../../App.css';
+import Cards from '../Cards.js'
+import Footer from "../Footer";
+
+function Home() {
+    return(
+        <>
+            <HeroSection />
+            <Cards />
+            <Footer />
+        </>
+    )
+}
+export default Home;
